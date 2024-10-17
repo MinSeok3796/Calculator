c079c52b43d5762707b9050f241e1bba969c5f32
MinSeok3796 <ms3796@naver.com>
2024-10-17 10:52:39 +0900

    Calculator Lv1

--- src/Calculator01/Main.java
+++ src/Calculator01/Main.java
@@ -9,9 +9,9 @@ public class Main {
             System.out.println("첫번째 수 : ");
             int num1 = sc.nextInt();
             System.out.println("두번째 수 : ");
-            int num2 = sc.nextInt();    // 1,2번 숫자 입력받기 완료
+            int num2 = sc.nextInt();
 
-            //사칙연산 입력받기
+
             System.out.println("수행하실 사칙연산을 입력해주세요 ");
             char operate = sc.next().charAt(0);
             switch (operate) {
@@ -34,7 +34,7 @@ public class Main {
                     System.out.println("제대로된 연산이 입력되지 않았습니다.");
                     break;
             }
-            System.out.println(num1 + " " + operate + " " + num2 + " = " + result);  // num1, operate, num2를 이어붙이게 되면 아스키 코드 값이 나타나는 현상이 있음
+            System.out.println(num1 + " " + operate + " " + num2 + " = " + result);  // num1, operate, num2를 이어붙이게 되면 아스키 코드 값이 나타나는 현상이 있었음
             sc.nextLine();
             System.out.println("더 하실래요?  ps.exit를 입력하시면 종료됩니다.");
             String answer = sc.nextLine();
